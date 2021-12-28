5351e8be60f98d64b2adf0311effbce7d3e2b1ad
finga <finga@onders.org>
2021-12-28 19:45:23 +0100

    Add sign and desk

---
+++ assets/tilesets/blonde-wood.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="blonde-wood" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <properties>
+  <property name="tilesetCopyright" value="CC-BY-SA (https://opengameart.org/content/lpc-wooden-furniture)"/>
+ </properties>
+ <image source="../alien/LPCwooden/blonde-wood.png" width="512" height="1024"/>
+</tileset>
